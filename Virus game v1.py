7f29196d3b7f7a6a59a7145518deb309de920209
iv18054 <79291090+iv18054@users.noreply.github.com>
2021-11-26 14:53:53 +1300

    Delete Virus game v1.py

--- Virus game v1.py
+++
@@ -1,8 +0,0 @@
-#Users input
-country = str(input("Enter a countries name"))
-#Users output
-print("The country is", country)
-
-number = int(input("Enter the amount of number you want for the country to be infected by"))
-
-print("The number of infected country is", number)
