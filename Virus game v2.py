0d4254e64220a973a99569cf7e8fa9133a930341
iv18054 <79291090+iv18054@users.noreply.github.com>
2021-11-26 14:54:35 +1300

    Add files via upload

---
+++ Virus game v2.py
@@ -0,0 +1,36 @@
+#List to hold all the countries
+country_list = []
+
+NZ = ("New Zealand")
+CH = ("China")
+JP = ("Japan")
+country_list.append(NZ)
+country_list.append(CH)
+country_list.append(JP)
+
+#Function to ask user which country to add
+def add_country():
+    #Loop keeps adding countries until the user is done
+    while True:
+        country = input("Which country would you like to add?")
+        if country != "done":
+            infected = int(input("What percent of the country is infected"))
+        #If user is done adding countries break out of function
+        else:
+            break
+        newlist = country
+        i = country_list.append(newlist)
+        for i in country_list:
+            print(i)
+            
+#Function to ask user if they will want to add a country
+def main():
+    country = input("If you would like to add in a country, type in \"add\" or type in \"done\" and press enter")
+    if country.lower() == "add":
+        add_country()
+    elif country.lower() == "done":
+        help()
+
+
+if __name__ == "__main__":
+    main()
